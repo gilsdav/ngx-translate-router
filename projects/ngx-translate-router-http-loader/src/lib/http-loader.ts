2402074438a262f24f2d7052acfb5a5a7b16d791
David Gilson <davgilson@live.fr>
2018-10-20 22:17:47 +0200

    add http loader

---
+++ projects/ngx-translate-router-http-loader/src/lib/http-loader.ts
@@ -0,0 +1,48 @@
+import { LocalizeParser, LocalizeRouterSettings } from '@gilsdav/ngx-translate-router';
+import { TranslateService } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
+import { Routes } from '@angular/router';
+import { Location } from '@angular/common';
+
+/**
+ * Config interface
+ */
+export interface ILocalizeRouterParserConfig {
+  locales: Array<string>;
+  prefix?: string;
+}
+
+export class LocalizeRouterHttpLoader extends LocalizeParser {
+  /**
+   * CTOR
+   * @param translate
+   * @param location
+   * @param settings
+   * @param http
+   * @param path
+   */
+  constructor(
+    translate: TranslateService,
+    location: Location,
+    settings: LocalizeRouterSettings,
+    private http: HttpClient,
+    private path: string = 'assets/locales.json'
+    ) {
+    super(translate, location, settings);
+  }
+
+  /**
+   * Initialize or append routes
+   * @param routes
+   */
+  load(routes: Routes): Promise<any> {
+    return new Promise((resolve: any) => {
+      this.http.get(`${this.path}`)
+        .subscribe((data: ILocalizeRouterParserConfig) => {
+          this.locales = data.locales;
+          this.prefix = data.prefix || '';
+          this.init(routes).then(resolve);
+        });
+    });
+  }
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Location } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 import { TranslateService } from '@ngx-translate/core';
 
 import { LocalizeRouterModule, LocalizeParser, ManualParserLoader, LocalizeRouterSettings } from '@gilsdav/ngx-translate-router';
+import { LocalizeRouterHttpLoader } from '@gilsdav/ngx-translate-router-http-loader';
 
 import { HomeComponent } from './home/home.component';
 
 
-export function ManualLoaderFactory(translate: TranslateService, location: Location, settings: LocalizeRouterSettings) {
-    return new ManualParserLoader(translate, location, settings, ['en', 'fr'], 'ROUTES.');
+// export function ManualLoaderFactory(translate: TranslateService, location: Location, settings: LocalizeRouterSettings) {
+//     return new ManualParserLoader(translate, location, settings, ['en', 'fr'], 'ROUTES.');
+// }
+
+export function HttpLoaderFactory(translate: TranslateService, location: Location, settings: LocalizeRouterSettings, http: HttpClient) {
+  return new LocalizeRouterHttpLoader(translate, location, settings, http);
 }
 
 export const routes: Routes = [
@@ -27,10 +33,15 @@ export const routes: Routes = [
     imports: [
         RouterModule.forRoot(routes),
         LocalizeRouterModule.forRoot(routes, {
+            // parser: {
+            //     provide: LocalizeParser,
+            //     useFactory: ManualLoaderFactory,
+            //     deps: [TranslateService, Location, LocalizeRouterSettings]
+            // }
             parser: {
-                provide: LocalizeParser,
-                useFactory: ManualLoaderFactory,
-                deps: [TranslateService, Location, LocalizeRouterSettings]
+              provide: LocalizeParser,
+              useFactory: HttpLoaderFactory,
+              deps: [TranslateService, Location, LocalizeRouterSettings, HttpClient]
             }
         })
     ],
