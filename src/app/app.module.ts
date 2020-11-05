5420a080fc15b40bab36a28688702c011b691680
David Gilson <davgilson@live.fr>
2020-11-05 23:04:57 +0100

    add hook and move initialNavigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, APP_INITIALIZER, Injector } from '@angular/core';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateModule, TranslateLoader, TranslateService } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { LocalizeRouterService } from '@gilsdav/ngx-translate-router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './app-routing.module';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { tap } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 export function createTranslateLoader(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/locales/', '.json');
 }
+
+// export const appInitializerFactory = (injector: Injector) => {
+//   return () => {
+//     const localize = injector.get(LocalizeRouterService);
+//     const router = injector.get(Router);
+//     return localize.hooks.initialized
+//       .pipe(
+//         tap(() => {
+//           console.log('Logic after initialization');
+//           router.navigate(['/fr/testounet/bobie']);
+//         })
+//       )
+//       .toPromise();
+//   }
+// };
 
 @NgModule({
   declarations: [
@@ -32,7 +50,14 @@ export function createTranslateLoader(http: HttpClient) {
         }
     })
   ],
-  providers: [],
+  // providers: [
+  //   {
+  //     provide: APP_INITIALIZER,
+  //     useFactory: appInitializerFactory,
+  //     deps: [ Injector ],
+  //     multi: true
+  //   }
+  // ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
