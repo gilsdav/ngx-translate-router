0f01e58a31525d13ec2018e63301ba1d370707f2
Amadou Sall <ahasall.dev@gmail.com>
2020-11-12 20:46:12 +0100

    chore: move to an Nx workspace

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  projects: [
+    '<rootDir>/apps/demo',
+    '<rootDir>/libs/core',
+    '<rootDir>/libs/http-loader',
+    '<rootDir>/libs/scully-plugin',
+  ],
+};
---
+++ libs/http-loader/README.md
@@ -0,0 +1,7 @@
+# http-loader
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test http-loader` to execute the unit tests.
